7b82e61c009401c2260135b12a2c2414fde273cd
rajkamal3 <rkchenumalla@gmail.com>
2021-06-17 20:47:54 +0530

    image compression implemented

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const path = require('path');
+const os = require('os');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const slash = require('slash');
 
 process.env.NODE_ENV = 'development';
 
@@ -94,8 +100,33 @@ const menu = [
 ];
 
 ipcMain.on('image:minimize', (e, options) => {
-    console.log(options);
+    options.dest = path.join(os.homedir(), 'imageshrink');
+    shrinkImage(options);
 });
+
+async function shrinkImage({ imgPath, quality, dest }) {
+    try {
+        const pngQuality = quality / 100;
+
+        const files = await imagemin([slash(imgPath)], {
+            destination: dest,
+            plugins: [
+                imageminMozjpeg({ quality }),
+                imageminPngquant({
+                    quality: [pngQuality, pngQuality]
+                })
+            ]
+        });
+
+        console.log(files);
+
+        shell.openPath(dest);
+
+        mainWindow.webContents.send('image:done');
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 app.on('window-all-closed', () => {
     if (!isMac) {
