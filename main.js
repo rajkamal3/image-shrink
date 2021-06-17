474a40d060b7f2bf54b2022148237b50496ed65e
rajkamal3 <rkchenumalla@gmail.com>
2021-06-17 18:40:41 +0530

    about window added

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
@@ -21,23 +22,60 @@ function createMainWindow() {
     mainWindow.loadFile('./app/index.html');
 }
 
+function createAboutWindow() {
+    aboutWindow = new BrowserWindow({
+        title: 'About',
+        width: '300',
+        height: '300',
+        resizable: false,
+        backgroundColor: 'white'
+    });
+
+    aboutWindow.loadFile('./app/about.html');
+}
+
 app.on('ready', () => {
     createMainWindow();
 
     const mainMenu = Menu.buildFromTemplate(menu);
     Menu.setApplicationMenu(mainMenu);
 
-    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
-    globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools());
+    // globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
+    // globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools());
 
     mainWindow.on('ready', () => (mainWindow = null));
 });
 
 const menu = [
-    ...(isMac ? [{ role: 'appMenu' }] : []),
+    ...(isMac
+        ? [
+              {
+                  label: app.name,
+                  submenu: [
+                      {
+                          label: 'About',
+                          click: createAboutWindow
+                      }
+                  ]
+              }
+          ]
+        : []),
     {
         role: 'fileMenu'
     },
+    ...(!isMac
+        ? [
+              {
+                  label: 'Help',
+                  submenu: [
+                      {
+                          label: 'About',
+                          click: createAboutWindow
+                      }
+                  ]
+              }
+          ]
+        : []),
     ...(isDev
         ? [
               {
